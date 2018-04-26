743fbfd8ee7181798216d2fc3ec6ca1af3bf4ff9
acorerhardell <acorhernandez@gmail.com>
2018-04-26 15:26:02 +0000

    impresion de temperatura en Farenheit

--- ejemplo-1-calentador.cpp
+++ ejemplo-1-calentador.cpp
@@ -9,7 +9,8 @@ public:
     Calentador();
     void calentar();
     void enfriar();
-    void imprimirTemperatura();
+    void imprimeCentigrados() const;
+    void imprimeFarenheit() const;
     int accedeTemperatura() const;
 };//punto y coma obligatorio
 
@@ -31,9 +32,9 @@ void Calentador::enfriar()
         temperatura-=incremento;}
 }
 
-void Calentador::imprimirTemperatura()
+void Calentador::imprimeCentigrados() const
 {
-    std::cout<<"La temperatura es: "<<temperatura<<std::endl;
+    std::cout<<"La temperatura es: "<<temperatura<<"°C"<<std::endl;
 }
 
 int Calentador::accedeTemperatura() const
@@ -41,19 +42,22 @@ int Calentador::accedeTemperatura() const
     return temperatura;
 }
 
+void Calentador::imprimeFarenheit() const
+{
+    float farenheit=temperatura*1.8+32;
+    std::cout <<"La temperatura es: " << farenheit<<"°F"<<std::endl;
+}
+
 int main()
 {
     Calentador c1;
     Calentador c2;
     
-    for(int i=0;i<10;i++){
-        c1.calentar();
-        c1.imprimirTemperatura();
-    }
-    
     c1.calentar();
-    c1.imprimirTemperatura();
+    c1.imprimeCentigrados();
+    c1.imprimeFarenheit();
     
     c2.enfriar();
-    c2.imprimirTemperatura();
+    c2.imprimeCentigrados();
+    c2.imprimeFarenheit();
 }
