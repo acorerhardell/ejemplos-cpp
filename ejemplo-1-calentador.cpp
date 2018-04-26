f796336f45899a0e28f7e13475c494a33bdc15b1
acorerhardell <acorhernandez@gmail.com>
2018-04-26 15:56:54 +0000

    agregar parametors al constructor y validaciones

--- ejemplo-1-calentador.cpp
+++ ejemplo-1-calentador.cpp
@@ -5,8 +5,10 @@ class Calentador
 private:
     int temperatura;
     int incremento;
+    int min;
+    int max;
 public:
-    Calentador();
+    Calentador(int min, int max,int temperatura=0);
     void calentar();
     void enfriar();
     void imprimeCentigrados() const;
@@ -14,21 +16,33 @@ public:
     int accedeTemperatura() const;
 };//punto y coma obligatorio
 
-Calentador::Calentador()
+Calentador::Calentador(int min, int max, int temperatura)
 {
+    /*if(min>max){
+        //std::cout<<"Error en el rango min-max"<<std::endl;
+        //std::exit(EXIT_FAILURE);
+        //throw "Error en el rango min-max";
+    }*/
+    
     incremento=3;
-    temperatura=15;
+    
+    if(temperatura>=min&&temperatura<=max){
+        this->temperatura=temperatura;}
+    else{
+        this->temperatura=min;}
+    this->min=min;
+    this->max=max;
 }
 
 void Calentador::calentar()
 {
-    if(temperatura+incremento<=30){
+    if(temperatura+incremento<=this->max){
         temperatura+=incremento;}
 }
 
 void Calentador::enfriar()
 {
-    if(temperatura-incremento<=-10){
+    if(temperatura-incremento>=this->min){
         temperatura-=incremento;}
 }
 
@@ -50,14 +64,18 @@ void Calentador::imprimeFarenheit() const
 
 int main()
 {
-    Calentador c1;
-    Calentador c2;
-    
-    c1.calentar();
-    c1.imprimeCentigrados();
-    c1.imprimeFarenheit();
-    
-    c2.enfriar();
-    c2.imprimeCentigrados();
-    c2.imprimeFarenheit();
+    try {
+        Calentador c1{-10,10};
+        
+        Calentador c2{30,10,0};
+        
+        c1.calentar();
+        c1.imprimeCentigrados();
+        c1.imprimeFarenheit();
+        
+        c2.enfriar();
+        c2.imprimeCentigrados();
+        c2.imprimeFarenheit();
+        }
+    catch (const std::runtime_error &e)
 }
